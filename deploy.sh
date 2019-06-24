82c9766e6a4b968aced5046a7a8792ff0ddb914d
Jehadsama <339364351@qq.com>
2019-06-24 16:34:48 +0800

    feat(deploy.sh): update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,9 @@ num=`expr $index - 1`
 IMAGE=${IMAGENAME:0:num}
 echo "IMAGE is ${IMAGE}"
 
-# docker login -u jehadsama -p l2598635
-# docker  pull $IMAGENAME
-# docker run --name $IMAGE -p 9000:9000 --restart always --network my-net -d $IMAGENAME
+docker login -u jehadsama -p l2598635
+docker  pull $IMAGENAME
+docker stop $IMAGE && docker rm $IMAGE
+docker run --name $IMAGE -p 9000:9000 --restart always --network my-net -d $IMAGENAME
 
 echo "deploy.sh end!"
