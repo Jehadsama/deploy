09fd016f2d498742159783b368e67f2d95f22473
Jehadsama <339364351@qq.com>
2020-08-26 18:12:04 +0800

    feat(.travis.yml,deploy.sh): shell

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,13 @@
+#!/bin/bash
 echo "Hello! Welcome to Jehadsama's service!"
-
 echo "deploy.sh start!"
 
-REPO=$1
-echo "REPO is $REPO"
-IMAGE=$2
-echo "IMAGE is $IMAGE"
-TAG=$3
-echo "TAG is $TAG"
-
-IMAGENAME=$REPO/$IMAGE:$TAG
+IMAGENAME=jehadsama/deploy
 
 docker login -u jehadsama -p l2598635
 docker  pull $IMAGENAME
 docker stop $IMAGE && docker rm $IMAGE
 docker run --name $IMAGE -p 9000:9000 --restart always --network my-net -d $IMAGENAME
 
+echo 'travis build done!'
 echo "deploy.sh end!"
