fb01ead1a544040ea5064ec9fe25ed0e849df43f
Jehadsama <339364351@qq.com>
2020-08-26 17:36:13 +0800

    feat(Dockerfile): multi build image

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM node
+FROM node As npminstall
 LABEL maintainer="Jehadsama<339364351@qq.com>"
 
-COPY ./ /src-app
-RUN npm config set registry https://registry.npm.taobao.org && \
-  cd /src-app && \
-  NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node npm i --production --verbose
+WORKDIR /tmp
+COPY package*.json ./
+ENV NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node NPM_CONFIG_REGISTRY=https://registry.npm.taobao.org
+RUN npm i --production --verbose
+
+FROM node:alpine
 WORKDIR /src-app
+COPY . ./
+COPY --from=npminstall /tmp/node_modules /src-app/node_modules
 EXPOSE 9000
-ENTRYPOINT ["npm", "run"]
-CMD ["start"]
+ENTRYPOINT ["node", "app.js"]
--- app.js
+++ app.js
@@ -1,28 +1,8 @@
-const util = require('util');
-const { exec } = require('child_process');
 const express = require('express');
-
-const execPromisify = util.promisify(exec);
-
-const runCmd = async (repo, name, tag = 'latest') => {
-  const { stdout, stderr } = await execPromisify(
-    `./deploy.sh ${repo} ${name} ${tag}`,
-  );
-  console.log('============');
-  console.log(stdout);
-  console.log('============');
-  console.log(stderr);
-};
 
 const app = express();
 app.get('/', async (req, res) => {
-  const { repo, name, tag = 'latest' } = req.query;
-  if (!repo || !name) {
-    res.send({ success: false, desc: 'invalid project name', code: 14000 });
-    return res.end();
-  }
-  await runCmd(repo, name, tag);
-  res.send({ success: true });
+  res.send({ success: true, data: 'hello, deploy successfully' });
   return res.end();
 });
 
